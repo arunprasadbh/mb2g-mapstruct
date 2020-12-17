2bf6bc45d98466a0bd34c3e2f47188470bc0197a
arunabhamidipati <arunprasad_bh@yahoo.com>
2020-12-17 18:58:20 +0000

    Adding gitignore, modifying pom

---
+++ src/main/java/guru/springframework/model/UserCommand.java
@@ -0,0 +1,15 @@
+package guru.springframework.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserCommand {
+    private String firstName;
+    private String lastName;
+    private String email;
+
+}
